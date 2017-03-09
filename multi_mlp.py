9f76d2179a714dc72c15cdecef5b17e77f401e5d
Adelmo Junior <noblehelm@gmail.com>
2017-03-09 19:48:14 -0300

    Fix normalizations and error calculation
    Normalization was fixed by separating a scaler for each observed
    variable, so they do not mess up the data. Error calc was fixed by
    denormalizing the data first (straight from both model.predict and the
    original data) and passing it as parameter to
    sklearn.metrics.mean_squared_error.

--- multi_mlp.py
+++ multi_mlp.py
@@ -10,15 +10,15 @@ from sklearn.metrics import mean_squared_error
 
 numpy.random.seed(7)
 
-dataframe = pandas.read_csv('data/data_selection.csv', usecols=['UmidadeRelativaMedia'], engine='python', skipfooter=3)
+dataframe = pandas.read_csv('data_selection.csv', usecols=['UmidadeRelativaMedia'], engine='python' )
 dataset_umidade_media = dataframe.values
 dataset_umidade_media = dataset_umidade_media.astype('float32')
 
-dataframe = pandas.read_csv('data/data_selection.csv', usecols=['TempMaximaMedia'], engine='python', skipfooter=3)
+dataframe = pandas.read_csv('data_selection.csv', usecols=['TempMaximaMedia'], engine='python')
 dataset_temp_max = dataframe.values
 dataset_temp_max = dataset_temp_max.astype('float32')
 
-dataframe = pandas.read_csv('data/data_selection.csv', usecols=['TempMinimaMedia'], engine='python', skipfooter=3)
+dataframe = pandas.read_csv('data_selection.csv', usecols=['TempMinimaMedia'], engine='python' )
 dataset_temp_min = dataframe.values
 dataset_temp_min = dataset_temp_min.astype('float32')
 
@@ -39,10 +39,20 @@ def normalize(dataset):
 #dataset_temp_max = normalize(dataset_temp_max)
 #dataset_temp_min = normalize(dataset_temp_min)
 
-scaler = MinMaxScaler(feature_range=(0,1))
-dataset_umidade_media = scaler.fit_transform(dataset_umidade_media)
-dataset_temp_max = scaler.fit_transform(dataset_temp_max)
-dataset_temp_min = scaler.fit_transform(dataset_temp_min)
+# criar escaladores
+umidade_media_scaler = MinMaxScaler(feature_range=(0,1))
+temp_max_scaler = MinMaxScaler(feature_range=(0,1))
+temp_min_scaler = MinMaxScaler(feature_range=(0,1))
+
+# calcular máximos e mínimos e armazenar em cache os transformadores
+umidade_media_scaler = umidade_media_scaler.fit(dataset_umidade_media)
+temp_max_scaler = temp_max_scaler.fit(dataset_temp_max)
+temp_min_scaler = temp_min_scaler.fit(dataset_temp_min)
+
+# transformar os dados (normalizacao)
+dataset_umidade_media = umidade_media_scaler.transform(dataset_umidade_media)
+dataset_temp_max = temp_max_scaler.transform(dataset_temp_max)
+dataset_temp_min = temp_min_scaler.transform(dataset_temp_min)
 
 # split into train and test sets
 train_size = int(len(dataset_temp_max) * 0.7)
@@ -84,7 +94,7 @@ temp_max_input = Input(shape=(look_back,),name='temp_max_input')
 temp_min_input = Input(shape=(look_back,),name='temp_min_input')
 
 merged = merge([umidade_media_input,temp_max_input,temp_min_input],mode='concat')
-x = Dense(8,activation=ativacao)(merged)
+x = Dense(1,activation=ativacao)(merged)
 
 temp_max_output = Dense(1,activation=ativacao, name='temp_max_output')(x)
 temp_min_output = Dense(1,activation=ativacao, name='temp_min_output')(x)
@@ -94,7 +104,7 @@ model = Model(input=[umidade_media_input,temp_max_input,temp_min_input], output=
 model.compile(loss='mean_squared_error', optimizer='adam')
 
 
-model.fit({ 'umidade_media_input': train_umidade_media_X ,'temp_max_input': train_temp_max_X, 'temp_min_input': train_temp_min_X}, {'temp_max_output': train_temp_max_Y, 'temp_min_output': train_temp_min_Y}, nb_epoch=10, batch_size=2, verbose=2)
+model.fit({ 'umidade_media_input': train_umidade_media_X ,'temp_max_input': train_temp_max_X, 'temp_min_input': train_temp_min_X}, {'temp_max_output': train_temp_max_Y, 'temp_min_output': train_temp_min_Y}, nb_epoch=100, verbose=2, batch_size=2)
 
 # funcao de convergencia por meio de early stopping
 def train_until_convergence(model,train_input,train_output):
@@ -121,44 +131,66 @@ def train_until_specific_error(model,train_input,train_output,error):
 #print( train_until_convergence(model,trainX,trainY) )
 
 
-
-# generate predictions for training
-trainPredict = model.predict({ 'umidade_media_input': train_umidade_media_X ,'temp_max_input': train_temp_max_X, 'temp_min_input': train_temp_min_X})
-testPredict = model.predict({'umidade_media_input':test_umidade_media_X,'temp_max_input':test_temp_max_X,'temp_min_input':test_temp_min_X})
+# gerar predições
+trainPredict = model.predict({'umidade_media_input': train_umidade_media_X ,'temp_max_input': train_temp_max_X, 'temp_min_input': train_temp_min_X},batch_size=2)
+testPredict = model.predict({'umidade_media_input':test_umidade_media_X,'temp_max_input':test_temp_max_X,'temp_min_input':test_temp_min_X},batch_size=2)
 
 # inverter as predições
-trainPredict = scaler.inverse_transform(trainPredict)
+# na saída de predict, [0] é temp_max e [1] é temp_min
+trainPredict[0] = temp_max_scaler.inverse_transform(trainPredict[0])
+trainPredict[1] = temp_min_scaler.inverse_transform(trainPredict[1])
+testPredict[0] = temp_max_scaler.inverse_transform(testPredict[0])
+testPredict[1] = temp_min_scaler.inverse_transform(testPredict[1])
 
+# inverter os conjuntos de dados originais
+train_temp_max_Y = temp_max_scaler.inverse_transform([train_temp_max_Y])
+train_temp_min_Y = temp_min_scaler.inverse_transform([train_temp_min_Y])
+test_temp_max_Y = temp_max_scaler.inverse_transform([test_temp_max_Y])
+test_temp_min_Y = temp_min_scaler.inverse_transform([test_temp_min_Y])
 
+# calcular MSE
+temp_max_trainScore = mean_squared_error(train_temp_max_Y[0],trainPredict[0]) 
+temp_min_trainScore = mean_squared_error(train_temp_min_Y[0],trainPredict[1]) 
+temp_max_testScore = mean_squared_error(test_temp_max_Y[0],testPredict[0]) 
+temp_min_testScore = mean_squared_error(test_temp_min_Y[0],testPredict[1]) 
+
+# print
+print('temp_max_trainScore: %.2f MSE' % (temp_max_trainScore))
+print('temp_min_trainScore: %.2f MSE' % (temp_min_trainScore))
+print('temp_max_testScore: %.2f MSE' % (temp_max_testScore))
+print('temp_min_testScore: %.2f MSE' % (temp_min_testScore))
+
+plt.plot(temp_max_scaler.inverse_transform(dataset_temp_max))
+plt.plot(temp_min_scaler.inverse_transform(dataset_temp_min))
 
 # shift train_temp_max predictions for plotting
-train_temp_max_PredictPlot = numpy.empty_like(dataset_umidade_media)
-train_temp_max_PredictPlot[:, :] = numpy.nan
-train_temp_max_PredictPlot[look_back:len(trainPredict[0])+look_back, :] = trainPredict[0]
+#train_temp_max_PredictPlot = numpy.empty_like(dataset_umidade_media)
+#train_temp_max_PredictPlot[:, :] = numpy.nan
+#train_temp_max_PredictPlot[look_back:len(trainPredict[0])+look_back, :] = trainPredict[0]
 
 # shift train_temp_min predictions for plotting
-train_temp_min_PredictPlot = numpy.empty_like(dataset_temp_max)
-train_temp_min_PredictPlot[:, :] = numpy.nan
-train_temp_min_PredictPlot[look_back:len(trainPredict[1])+look_back, :] = trainPredict[1]
+#train_temp_min_PredictPlot = numpy.empty_like(dataset_temp_max)
+#train_temp_min_PredictPlot[:, :] = numpy.nan
+#train_temp_min_PredictPlot[look_back:len(trainPredict[1])+look_back, :] = trainPredict[1]
 
 # shift test_temp_max predictions for plotting
-test_temp_max_PredictPlot = numpy.empty_like(dataset_umidade_media)
-test_temp_max_PredictPlot[:, :] = numpy.nan
-test_temp_max_PredictPlot[len(trainPredict[0])+(look_back*2)+1:len(dataset_umidade_media)-1, :] = testPredict[0]
+#test_temp_max_PredictPlot = numpy.empty_like(dataset_umidade_media)
+#test_temp_max_PredictPlot[:, :] = numpy.nan
+#test_temp_max_PredictPlot[len(trainPredict[0])+(look_back*2)+1:len(dataset_umidade_media)-1, :] = testPredict[0]
 
 # shift test_temp_min predictions for plotting
-test_temp_min_PredictPlot = numpy.empty_like(dataset_temp_max)
-test_temp_min_PredictPlot[:, :] = numpy.nan
-test_temp_min_PredictPlot[len(trainPredict[1])+(look_back*2)+1:len(dataset_temp_max)-1, :] = testPredict[1]
+#test_temp_min_PredictPlot = numpy.empty_like(dataset_temp_max)
+#test_temp_min_PredictPlot[:, :] = numpy.nan
+#test_temp_min_PredictPlot[len(trainPredict[1])+(look_back*2)+1:len(dataset_temp_max)-1, :] = testPredict[1]
 
 # plot baseline and predictions
-plt.plot(dataset_umidade_media)
-plt.plot(dataset_temp_max)
+#plt.plot(dataset_umidade_media)
+#plt.plot(dataset_temp_max)
 
-plt.plot(train_temp_max_PredictPlot)
-plt.plot(train_temp_min_PredictPlot)
-plt.plot(test_temp_max_PredictPlot)
-plt.plot(test_temp_min_PredictPlot)
+#plt.plot(train_temp_max_PredictPlot)
+#plt.plot(train_temp_min_PredictPlot)
+#plt.plot(test_temp_max_PredictPlot)
+#plt.plot(test_temp_min_PredictPlot)
 
 
 plt.xlabel("Observações")
