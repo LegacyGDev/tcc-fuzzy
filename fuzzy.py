bf5b70bad26df4400192e61e61fab2f2de55092c
Adelmo Junior <noblehelm@gmail.com>
2017-03-22 21:05:03 -0300

    fix wrong fuzzy region division

--- fuzzy.py
+++ fuzzy.py
@@ -9,12 +9,12 @@ def divide_into_fuzzy_regions(variable,n):
     regions = []
     num_regions = (2*n)+1
     interval = (min(variable),max(variable))
-    region_length = (interval[1] - interval[0])/num_regions
+    region_length = (interval[1] - interval[0])/n
     for i in range(num_regions):
         if(i==0):
-            regions.append((interval[0],interval[0],interval[0]+region_length))
+            regions.append((interval[0],interval[0],interval[0]+(region_length/2)))
         elif(i==num_regions-1):
-            regions.append((interval[1]-region_length,interval[1],interval[1]))
+            regions.append((interval[1]-(region_length/2),interval[1],interval[1]))
         else:
             lower_bound = interval[0] + (region_length * (i-1))
             regions.append( ( lower_bound , lower_bound + (region_length/2) , lower_bound + region_length ) )
