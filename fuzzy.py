a0f6d06390277286ae733bfe90128ef1ef6fb22b
Adelmo Junior <noblehelm@gmail.com>
2017-03-23 00:12:00 -0300

    fix division lower bound and degree array problem

--- fuzzy.py
+++ fuzzy.py
@@ -1,6 +1,7 @@
 import pandas
 import matplotlib.pyplot as plt
 import skfuzzy as fuzz
+import numpy as np
 
 #dataset = pandas.read_csv("Dads.csv", usecols=['Temperatura'])
 #dataset = dataset['Temperatura']
@@ -16,14 +17,14 @@ def divide_into_fuzzy_regions(variable,n):
         elif(i==num_regions-1):
             regions.append((interval[1]-(region_length/2),interval[1],interval[1]))
         else:
-            lower_bound = interval[0] + (region_length * (i-1))
+            lower_bound = interval[0] + ((region_length/2) * (i-1))
             regions.append( ( lower_bound , lower_bound + (region_length/2) , lower_bound + region_length ) )
     return regions
 
 def determine_degrees_and_assign(x,regions):
     degrees = []
     for r in regions:
-        degrees.append(fuzz.trimf(x,r))
+    	degrees.append(fuzz.trimf(np.asarray([x]),r))
     assignment = {}
-    assignment['x'] = regions[(max(degrees)).index()]
+    assignment[x] = regions[np.argmax(degrees)]
     return assignment
