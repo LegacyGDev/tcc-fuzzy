87cdf1a7de1f82e297c25cb39e6c8e82efe099d6
Adelmo Junior <noblehelm@gmail.com>
2017-03-24 00:36:38 -0300

    add fuzzy region division with labels

--- fuzzy.py
+++ fuzzy.py
@@ -21,6 +21,26 @@ def divide_into_fuzzy_regions(variable,n):
             regions.append( ( lower_bound , lower_bound + (region_length/2) , lower_bound + region_length ) )
     return regions
 
+def divide_into_fuzzy_regions_and_label(variable,n):
+    regions = {}
+    num_regions = (2*n)+1
+    interval = (min(variable),max(variable))
+    region_length = (interval[1] - interval[0])/n
+    for i in range(num_regions):
+        if(i==0):
+            regions["S{}".format(n)] = (interval[0],interval[0],interval[0]+(region_length/2))
+        elif(i==num_regions-1):
+            regions["B{}".format(n)] = (interval[1]-(region_length/2),interval[1],interval[1])
+        else:
+            lower_bound = interval[0] + ((region_length/2) * (i-1))
+            if(i < n):
+            	regions["S{}".format(n-i)] = ( lower_bound , lower_bound + (region_length/2) , lower_bound + region_length )
+            elif(i > n):
+            	regions["B{}".format(i-n)] = ( lower_bound , lower_bound + (region_length/2) , lower_bound + region_length )
+            elif(i == n):
+            	regions["CE"] = ( lower_bound , lower_bound + (region_length/2) , lower_bound + region_length )
+    return regions
+
 def determine_degrees_and_assign(x,regions):
     degrees = []
     for r in regions:
@@ -28,3 +48,12 @@ def determine_degrees_and_assign(x,regions):
     assignment = {}
     assignment[x] = regions[np.argmax(degrees)]
     return assignment
+
+def generate_fuzzy_rule(inputs,outputs,regions):
+	rule = []
+#	for i in inputs:
+#		for j in i:
+#			determine_degrees_and_assign(j,
+
+#fuzzy_rule_base = ctrl.ControlSystem()
+#fuzzy_rule_base.add( ctrl.Rule( inputs, outputs ) )
