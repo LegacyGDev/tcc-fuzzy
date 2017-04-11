aa964d2cae199ea2157b614b6bb707e1c04f6eb4
Adelmo Junior <noblehelm@gmail.com>
2017-04-11 18:32:17 -0300

    merge both divide_into_fuzzy_regions functions

--- fuzzy.py
+++ fuzzy.py
@@ -3,39 +3,34 @@ import matplotlib.pyplot as plt
 import skfuzzy as fuzz
 import numpy as np
 
-def divide_into_fuzzy_regions(variable,n):
+def divide_into_fuzzy_regions(variable,n,label=True):
     regions = []
     num_regions = (2*n)+1
     interval = (min(variable),max(variable))
     region_length = (interval[1] - interval[0])/n
-    for i in range(num_regions):
-        if(i==0):
-            regions.append((interval[0],interval[0],interval[0]+(region_length/2)))
-        elif(i==num_regions-1):
-            regions.append((interval[1]-(region_length/2),interval[1],interval[1]))
-        else:
-            lower_bound = interval[0] + ((region_length/2) * (i-1))
-            regions.append( ( lower_bound , lower_bound + (region_length/2) , lower_bound + region_length ) )
-    return regions
-
-def divide_into_fuzzy_regions_and_label(variable,n):
-    regions = {}
-    num_regions = (2*n)+1
-    interval = (variable.min(),variable.max())
-    region_length = (interval[1] - interval[0])/n
-    for i in range(num_regions):
-        if(i==0):
-            regions["S{}".format(n)] = (interval[0],interval[0],interval[0]+(region_length/2))
-        elif(i==num_regions-1):
-            regions["B{}".format(n)] = (interval[1]-(region_length/2),interval[1],interval[1])
-        else:
-            lower_bound = interval[0] + ((region_length/2) * (i-1))
-            if(i < n):
-            	regions["S{}".format(n-i)] = ( lower_bound , lower_bound + (region_length/2) , lower_bound + region_length )
-            elif(i > n):
-            	regions["B{}".format(i-n)] = ( lower_bound , lower_bound + (region_length/2) , lower_bound + region_length )
-            elif(i == n):
-            	regions["CE"] = ( lower_bound , lower_bound + (region_length/2) , lower_bound + region_length )
+    if label:
+        for i in range(num_regions):
+            if(i==0):
+                regions["S{}".format(n)] = (interval[0],interval[0],interval[0]+(region_length/2))
+            elif(i==num_regions-1):
+                regions["B{}".format(n)] = (interval[1]-(region_length/2),interval[1],interval[1])
+            else:
+                lower_bound = interval[0] + ((region_length/2) * (i-1))
+                if(i < n):
+            	    regions["S{}".format(n-i)] = ( lower_bound , lower_bound + (region_length/2) , lower_bound + region_length )
+                elif(i > n):
+            	    regions["B{}".format(i-n)] = ( lower_bound , lower_bound + (region_length/2) , lower_bound + region_length )
+                elif(i == n):
+            	    regions["CE"] = ( lower_bound , lower_bound + (region_length/2) , lower_bound + region_length )
+    else:
+        for i in range(num_regions):
+            if(i==0):
+                regions.append((interval[0],interval[0],interval[0]+(region_length/2)))
+            elif(i==num_regions-1):
+                regions.append((interval[1]-(region_length/2),interval[1],interval[1]))
+            else:
+                lower_bound = interval[0] + ((region_length/2) * (i-1))
+                regions.append( ( lower_bound , lower_bound + (region_length/2) , lower_bound + region_length ) )
     return regions
 
 def determine_degrees_and_assign(x,regions,only_regions=False):
