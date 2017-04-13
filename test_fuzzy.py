a1141dfbb9de53328b0afa427c43f0ffa3c8629a
Adelmo Junior <noblehelm@gmail.com>
2017-04-13 14:01:34 -0300

    add determine_degree test cases

--- test_fuzzy.py
+++ test_fuzzy.py
@@ -6,9 +6,19 @@ import fuzzy
 class DivideIntoFuzzyRegionsTestCase(unittest.TestCase):
     def test_is_output_a_list(self):
         self.assertIs(type(fuzzy.divide_into_fuzzy_regions([0,1,2,3,4,5,6,7,8,9],1)),type([]))
+
     def test_is_output_len(self):
         for i in range(5):
             self.assertEqual(len(fuzzy.divide_into_fuzzy_regions([0,1,2,3,4,5,6,7,8,9],i+1)),(2*(i+1))+1)
+
+
+class DetermineDegreeAndAssignTestCase(unittest.TestCase):
+    def test_is_output_a_tuple(self):
+        self.assertIs(type(fuzzy.determine_degrees_and_assign(4,[(0,0,4.5),(0,4.5,9),(4.5,9,9)])),type(()))
+
+    def test_is_only_region_output_a_tuple(self):
+        self.assertIs(type(fuzzy.determine_degrees_and_assign(4,[(0,0,4.5),(0,4.5,9),(4.5,9,9)],only_regions=True)),type(()))
+
 
 if __name__ == '__main__':
     unittest.main()
